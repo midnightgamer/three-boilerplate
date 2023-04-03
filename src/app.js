bd1cd287736d2729125f3b3303ba413786333a1c
Pankaj <jaiswal1999.pj@gmail.com>
2023-04-03 22:17:51 +0530

    fixed folder

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
-
+import vertexShader from "./shaders/vertex.glsl";
+import fragmentShader from "./shaders/fragment.glsl";
 // init
 
 const camera = new THREE.PerspectiveCamera(
@@ -12,13 +13,16 @@ camera.position.z = 1;
 
 const scene = new THREE.Scene();
 
-const geometry = new THREE.PlaneGeometry(1,1);
-const material = new THREE.MeshNormalMaterial();
+const geometry = new THREE.PlaneGeometry(1, 1);
+const material = new THREE.ShaderMaterial({
+  fragmentShader,
+  vertexShader,
+  uniforms: { progress: { type: "f", value: 0 } },
+});
 material.side = THREE.DoubleSide;
 
-const mesh = new THREE.Mesh(geometry, material);
+const mesh = new THREE.Points(geometry, material);
 scene.add(mesh);
-
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
